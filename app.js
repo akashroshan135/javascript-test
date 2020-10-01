cf9c01a3264a49cb0a8ebbc3337330df29957601
Akash Roshan <akash.second1999@gmail.com>
2020-10-01 21:23:33 +0530

    add string concatenation

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
 const msg = "Hello World!!!";
 
-console.log(msg);
+const x = "test program";
 
-function addNum(num1, num2) {
-    console.log(num1 + num2);
-}
-
-const toUpper = (text) => {
-    text = text.toUpperCase();
-    console.log(text);
-};
-
-addNum(5, 10);
-toUpper(msg);
+console.log(`${msg}, This is a ${x}`);
