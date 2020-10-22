eef015bff1c61c7de63ef62c28159b0251aec3a7
Akash Roshan <akash.second1999@gmail.com>
2020-10-22 09:45:26 +0530

    testing async function

--- app.js
+++ app.js
@@ -1,29 +1,6 @@
-console.log("Hello World!!!");
-
-const title = document.querySelector("h1");
-const btn = document.getElementById("btn");
-btn.addEventListener("click", () => {
-    title.classList.toggle("title");
-});
-
-const testList = document.querySelectorAll(".testlist li");
-for (item of testList) {
-    item.addEventListener("click", function () {
-        console.log(this);
-        this.style.color = "pink";
-    });
+async function getText() {
+    let response = await fetch("test.txt");
+    let data = await response.text();
+    document.write(`Text from doc: ${data}`);
 }
-console.log(testList);
-
-const newList = document.querySelector(".newlist");
-const listInput = document.getElementById("listInput");
-const inputBtn = document.getElementById("btn1");
-inputBtn.addEventListener("click", function () {
-    //create li tag
-    const newItem = document.createElement("li");
-    const liContent = document.createTextNode(listInput.value);
-    //attach licontent inside li tag
-    newItem.appendChild(liContent);
-    //attach li tag inside namelist dom
-    newList.appendChild(newItem);
-});
+getText()
